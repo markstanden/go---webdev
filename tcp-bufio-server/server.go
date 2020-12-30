295a456a86e19ea7c965ea975c3df33d06ee3b09
Mark Standen <mark@markstanden.uk>
2020-12-30 20:25:12 +0000

    server updated with connection timeout

--- tcp-bufio-server/server.go
+++ tcp-bufio-server/server.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"time"
+)
+
+const (
+	timeout = time.Second * 10
 )
 
 func main() {
@@ -12,7 +18,7 @@ func main() {
 	port := "8080"
 	lisTCP, err := net.Listen("tcp", ":" + port)
 	if err != nil {
-		log.Panic("Error starting TCP server on port %v: %v", port, err)
+		log.Panicf("Error starting TCP server on port %v: %v", port, err)
 	}
 	// close listener when finished
 	defer lisTCP.Close()
@@ -33,19 +39,23 @@ func main() {
 	}
 }
 
-func handle(tcp net.Conn) {
+func handle(tcpConn net.Conn) {
+	err := tcpConn.SetDeadline(time.Now().Add(timeout))
+	if err != nil {
+		log.Println("Connection Timeout")
+	}
 	// bufio.NewScanner returns a pointer to a bufio.Scanner struct
-	scanner := bufio.NewScanner(tcp)
+	scanner := bufio.NewScanner(tcpConn)
 	
 	// scanner.Scan() advances the scanner to the next token (default is new line char)
 	// returns true is successful, false if EOF
 	for scanner.Scan() {
 		line := scanner.Text() // could also use scanner.Bytes()
 		log.Println(line) // print the line of text to the Stdout
+		fmt.Fprintf(tcpConn, "Line Read ok.")
+
 	}
 
 	// close the tcp connection
-	defer tcp.Close();
-
-
+	defer tcpConn.Close()
 }
